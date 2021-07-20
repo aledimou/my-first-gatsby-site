5f8525a4d8249df51b97b6c0830ce8624b438083
Your Name <you@example.com>
2021-07-20 18:55:20 +0300

    Finished Section 3

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,17 @@
 import * as React from 'react'
 import Layout from '../components/layout'
+import { StaticImage } from 'gatsby-plugin-image'
+const url = "https://images.unsplash.com/photo-1588195538326-c5b1e9f80a1b?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=947&q=80";
 
 const IndexPage = () => {
+
   return (
     <Layout pageTitle="Home Page">
       <p>I'm making this by following the Gatsby Tutorial.</p>
+      <h1>Wonderfull Cake!</h1>
+      <StaticImage src = {url} alt = "Cake" />
+      <h1>Another Cake!</h1>
+      <StaticImage src = "../images/Cake.jpg" alt = "Cake Brown" />
     </Layout>
   )
 }
